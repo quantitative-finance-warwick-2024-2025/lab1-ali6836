69c3d20c7dd15e3a4467189a3c7258a60ef89e48
ali6836 <tigerali1@yahoo.co.uk>
2024-10-14 15:07:10 +0100

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,10 @@ int main() {
   // you can call any function you like here
   // to test the output.
   exercise_1();
+  exercise_2(5, 4);
+  exercise_3();
+  exercise_4(2);
+  exercise_5();
+  exercise_6();
   return 0;
 }
